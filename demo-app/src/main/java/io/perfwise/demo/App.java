f28a9c67850ea69aa552b028859358519320056a
Khader Mohaideen <2052210@cognizant.com>
2021-10-05 10:56:14 +0100

    added demo app

---
+++ demo-app/src/main/java/io/perfwise/demo/App.java
@@ -0,0 +1,23 @@
+package io.perfwise.demo;
+
+public class App {
+
+	
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int div(int a, int b) {
+		return a / b;
+	}
+
+}
